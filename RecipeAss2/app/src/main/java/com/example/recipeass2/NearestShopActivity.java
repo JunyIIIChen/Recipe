5f4eeedf13efa01743db2c2f95d2434888ca6fab
mtan0143 <mtan0143@student.monash.edu>
2023-05-03 22:46:50 +1000

    实现了nearest shop screen,且实现了Map

---
+++ RecipeAss2/app/src/main/java/com/example/recipeass2/NearestShopActivity.java
@@ -0,0 +1,83 @@
+package com.example.recipeass2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.recipeass2.databinding.ActivityNearestShopBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class NearestShopActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private ActivityNearestShopBinding binding;
+
+    private MapView mapView;
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityNearestShopBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        goBackPrevScreen();
+    }
+
+    public void goBackPrevScreen() {
+        binding.topBar.goBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /**
+                 * 把 MainActivity 换成需要返回的screen
+                 * **/
+                //Intent intent = new Intent(NearestShopActivity.this, MainActivity.class);
+                //startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+}
