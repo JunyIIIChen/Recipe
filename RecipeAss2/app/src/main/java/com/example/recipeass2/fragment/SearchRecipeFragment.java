b941f65a82c9ab053ca1e7ca010b844a560beec3
Shuhai Yu <syuu0051@student.monash.edu>
2023-05-12 02:04:50 +1000

    change search and shopping list activity to fragment

---
+++ RecipeAss2/app/src/main/java/com/example/recipeass2/fragment/SearchRecipeFragment.java
@@ -0,0 +1,98 @@
+package com.example.recipeass2.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.recipeass2.databinding.SearchRecipeFragmentBinding;
+import com.example.recipeass2.recipe.RecipeActivity;
+import com.example.recipeass2.search.Recipe;
+import com.example.recipeass2.search.RecipeAdapter;
+import com.example.recipeass2.search.RecipeSearchResponse;
+import com.example.recipeass2.search.SpoonacularApiService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class SearchRecipeFragment extends Fragment implements RecipeAdapter.OnRecipeClickListener{
+    private SearchRecipeFragmentBinding binding;
+    private RecipeAdapter recipeAdapter;
+    private static final String API_KEY = "c92ff870e8ae441ba53380608f13ed3c";
+    private static final String BASE_URL = "https://api.spoonacular.com/";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = SearchRecipeFragmentBinding.inflate(inflater, container, false);
+
+        recipeAdapter = new RecipeAdapter(this);
+        binding.searchResultsRecyclerview.setAdapter(recipeAdapter);
+        binding.searchResultsRecyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        binding.searchEdittext.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                searchRecipes(s.toString());
+            }
+        });
+
+        return binding.getRoot();
+    }
+
+    private void searchRecipes(String query) {
+        // Create a Retrofit instance for API requests
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        SpoonacularApiService spoonacularApiService = retrofit.create(SpoonacularApiService.class);
+
+        // Make an API request using the query string
+        Call<RecipeSearchResponse> call = spoonacularApiService.searchRecipes(API_KEY, query);
+
+        call.enqueue(new Callback<RecipeSearchResponse>() {
+            @Override
+            public void onResponse(Call<RecipeSearchResponse> call, Response<RecipeSearchResponse> response) {
+                if (response.isSuccessful()) {
+                    List<Recipe> recipes = response.body().getResults();
+                    recipeAdapter.setRecipes(recipes);
+                } else {
+                    // Show an error message or handle the error
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
+                // Show an error message or handle the error
+            }
+        });
+    }
+
+    public void onRecipeClick(Recipe recipe) {
+        Intent intent = new Intent(getActivity(), RecipeActivity.class);
+        intent.putExtra("recipe_id", recipe.getId()); // Pass the recipe ID
+        startActivity(intent);
+    }
+}
--- RecipeAss2/app/src/main/java/com/example/recipeass2/fragment/ShoppingListFragment.java
+++ RecipeAss2/app/src/main/java/com/example/recipeass2/fragment/ShoppingListFragment.java
@@ -4,35 +4,92 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 
+import com.example.recipeass2.database.AppDatabase;
 import com.example.recipeass2.databinding.ShoppingListFragmentBinding;
+import com.example.recipeass2.shoppingList.ShoppingListAdapter;
+import com.example.recipeass2.shoppingList.ShoppingListItem;
+import com.example.recipeass2.shoppingList.ShoppingListViewModel;
 import com.example.recipeass2.viewmodel.SharedViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShoppingListFragment extends Fragment {
     private ShoppingListFragmentBinding binding;
-    public ShoppingListFragment(){}
+    private ShoppingListAdapter adapter;
+    private List<ShoppingListItem> shoppingList;
+    private AppDatabase db;
+    private ShoppingListViewModel shoppingListViewModel;
+
+    //current screen name
+    private final String CURRENT_SCREEN_NAME = "Shopping list";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the View for this fragment using the binding
         binding = ShoppingListFragmentBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
-        SharedViewModel model = new
-                ViewModelProvider(requireActivity()).get(SharedViewModel.class);
-        model.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        db = Room.databaseBuilder(getActivity().getApplicationContext(),
+                        AppDatabase.class, "shopping_list_database")
+                .fallbackToDestructiveMigration()
+                .build();
+
+        // Initialize ViewModel
+        shoppingListViewModel = new ViewModelProvider(this).get(ShoppingListViewModel.class);
+
+//        // Set current screen name
+//        binding.topBar.currentScreenName.setText(CURRENT_SCREEN_NAME);
+
+        binding.shopListRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        adapter = new ShoppingListAdapter(new ArrayList<>(), db);
+        binding.shopListRecyclerview.setAdapter(adapter);
+
+        // Observe shopping list items
+        shoppingListViewModel.getAllShoppingListItems().observe(getViewLifecycleOwner(), new Observer<List<ShoppingListItem>>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                binding.textMessage.setText(s);
+            public void onChanged(List<ShoppingListItem> shoppingListItems) {
+                adapter.notifyDataSetChanged();
+                adapter.setShoppingList(shoppingListItems);
             }
         });
+        binding.clearShopListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearShopList();
+            }
+        });
+
+//        goBackPrevScreen();
+
         return view;
     }
+
+//    private void goBackPrevScreen(){
+//        binding.topBar.goBackButton.setOnClickListener(view -> {
+//            // ...
+//        });
+//    }
+
+    private void clearShopList(){
+        binding.clearShopListButton.setOnClickListener(view -> {
+            shoppingListViewModel.deleteAllShoppingListItems();
+            Toast.makeText(getActivity(), "Shopping list cleared.", Toast.LENGTH_SHORT).show();
+        });
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
